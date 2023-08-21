0ea44ec5f372eff2e4c656be483cc5add028d7ce
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-08-21 17:11:25 +1000

    ✨ 67 cav2 upgrade enable nullable types (#70)
    * Enabled nullability in all projects.
    
    * Fixed a few warnings

--- Src/Domain/Common/AuditableEntity.cs
+++ Src/Domain/Common/AuditableEntity.cs
@@ -4,11 +4,11 @@ namespace Northwind.Domain.Common;
 
 public class AuditableEntity
 {
-    public string CreatedBy { get; set; }
+    public string CreatedBy { get; set; } = null!;
 
     public DateTime Created { get; set; }
 
-    public string LastModifiedBy { get; set; }
+    public string? LastModifiedBy { get; set; }
 
     public DateTime? LastModified { get; set; }
 }
