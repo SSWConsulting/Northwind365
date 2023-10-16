0e964a4aa48d91eb795fdb23c3bd290110f6b174
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-10-17 09:40:20 +1000

    Upgraded more GA packages

--- Src/WebUI/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Src/WebUI/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,16 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using Northwind.Infrastructure.Identity;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Text.Encodings.Web;
-using System.Threading.Tasks;
 
 namespace Northwind.WebUI.Areas.Identity.Pages.Account;
 
