06c09bce6e8e69a545f9658afbd637b2497bbd7d
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-10-17 09:47:33 +1000

    Upgraded to .NET 8 rc2.

--- Src/WebUI/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Src/WebUI/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Northwind.Infrastructure.Identity;
