65f08511a05d4120854ddda6173d2f889d90e7c4
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-10-18 15:50:19 +1000

    Removed old ClientApp

--- Src/WebUI/ClientAppOld/src/app/nav-side-menu/nav-side-menu.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-nav-side-menu',
-  templateUrl: './nav-side-menu.component.html'
-})
-export class NavSideMenuComponent {
-}
--- Src/WebUI/ClientAppOld/src/app/nav-top-menu/nav-top-menu.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-nav-top-menu',
-  templateUrl: './nav-top-menu.component.html'
-})
-export class NavTopMenuComponent {
-}
