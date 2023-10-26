8566dfb94d5e6d385b3b4ff973fcd6e9641fd22d
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-10-26 14:32:33 +1000

    Attempt at fixing issue

--- Src/Infrastructure/Persistence/NorthwindDbContextFactory.cs
+++ Src/Infrastructure/Persistence/NorthwindDbContextFactory.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Northwind.Application.Common.Interfaces;
+using Northwind.Infrastructure.Services;
 
 namespace Northwind.Infrastructure.Persistence;
 
-// public class NorthwindDbContextFactory : DesignTimeDbContextFactoryBase<NorthwindDbContext>
-// {
-//     protected override NorthwindDbContext CreateNewInstance(DbContextOptions<NorthwindDbContext> options)
-//     {
-//         return new NorthwindDbContext(options);
-//     }
-// }
+public class NorthwindDbContextFactory : DesignTimeDbContextFactoryBase<NorthwindDbContext>
+{
+    protected override NorthwindDbContext CreateNewInstance(DbContextOptions<NorthwindDbContext> options)
+    {
+        return new NorthwindDbContext(options, new DesignTimeUserService(), new MachineDateTime());
+    }
+}
+
+internal class DesignTimeUserService : ICurrentUserService
+{
+    public string GetUserId()
+    {
+        return "00000000-0000-0000-0000-000000000000";
+    }
+}
