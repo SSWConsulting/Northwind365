4ea8b5827a6baab200aa777bbe4c2f8c7df9dd29
Matt Goldman <matt.goldman@outlook.com>
2023-11-16 14:07:00 +1100

    Added logout message #106

--- Src/WebUI/ClientApp/src/app/home/home.component.ts
+++ Src/WebUI/ClientApp/src/app/home/home.component.ts
@@ -1,7 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+declare var bootstrap: any;
 
 @Component({
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"]
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit
+{
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      if (params["loggedOut"] === "true") {
+        this.showLoggedOutToast();
+      }
+    });
+  }
+
+  showLoggedOutToast() {
+    const toastRef = document.getElementById("logoutToast");
+    const toast = bootstrap.Toast.getOrCreateInstance(toastRef);
+    toast.show();
+  }
+
+}
